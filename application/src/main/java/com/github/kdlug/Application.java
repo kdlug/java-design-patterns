1605b87b182b46dda133d438756140d778a93118
kdlug <casi@o2.pl>
2019-10-18 15:28:58 +0200

    Facade init

--- application/src/main/java/com/github/kdlug/Application.java
+++ application/src/main/java/com/github/kdlug/Application.java
@@ -1,5 +1,7 @@
 package com.github.kdlug;
 
+import com.github.kdlug.deliverybox.DeliveryBox;
+import com.github.kdlug.deliverybox.DeliveryBoxSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,17 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		pickupPackage();
 	}
 
+	private static void pickupPackage() {
+		DeliveryBox deliveryBox = new DeliveryBox();
+		DeliveryBoxSystem deliveryBoxSystem = new DeliveryBoxSystem();
+
+		deliveryBox.getUserData();
+		if (deliveryBoxSystem.isUserDataValidated() && deliveryBoxSystem.isPaymentSecured()) {
+			deliveryBox.openBox();
+		}
+	}
 }
---
+++ application/src/main/java/com/github/kdlug/deliverybox/DeliveryBox.java
@@ -0,0 +1,21 @@
+package com.github.kdlug.deliverybox;
+
+public class DeliveryBox {
+    public boolean isDeliveryBoxFull() {
+        System.out.println("Delivery Box is not full");
+        return false;
+    }
+
+    public boolean isDeliveryBoxBroken() {
+        System.out.println("Delivery box is not broken");
+        return false;
+    }
+
+    public void getUserData() {
+        System.out.println("User data entered");
+    }
+
+    public void openBox() {
+        System.out.println("Box opened");
+    }
+}
